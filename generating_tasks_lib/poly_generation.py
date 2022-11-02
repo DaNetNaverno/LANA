c8740e30fc8b33a5510be85db37f898a79238ccd
Eugene <eugmoor@gmail.com>
2022-11-02 21:32:23 +0300

    Обновлено multiplicity. Тип теперь dict Добавлен файл, где можно вводить значения для генерации

--- generating_tasks_lib/poly_generation.py
+++ generating_tasks_lib/poly_generation.py
@@ -1,31 +1,57 @@
 import random
+import poly_settings
 from fractions import Fraction
-from numpy import convolve, ndarray
+from numpy import convolve
 
 
 class PolynomGeneration:
-    def __init__(self, degree: int, roots: list, rational_coefs: bool, multiplicity: int, canon_view: bool,
-                 variable: str):
-        self.__degree = degree  # макс. степень полинома
-        self.__roots = roots  # введенные корни (вводить только с типом str!)
-        self.__rational_coefs = rational_coefs  # наличие рациональных коэффициентов
-        self.__multiplicity = multiplicity  # (x-a)^k отвечает за k
-        self.__canon_view = canon_view  # канонический вид
-        self.__variable = variable if variable == 'x' else '(' + variable + ')'  # переменная, по дефолту 'x'
+    def __init__(
+        self,
+        degree: int,
+        roots: list,
+        rational_coefs: bool,
+        multiplicity: dict,
+        canon_view: bool,
+        variable: str,
+    ):
+        self._degree = degree  # макс. степень полинома
+        self._roots = roots  # введенные корни (вводить только с типом str!)
+        self._rational_coefs = rational_coefs  # наличие рациональных коэффициентов
+        self._multiplicity = multiplicity  # (x-a)^k отвечает за k {'корень'(str): степень(int)}
+        self._canon_view = canon_view  # канонический вид
+        self._variable = (
+            variable if variable == "x" else "(" + variable + ")"
+        )  # переменная, по дефолту 'x'
 
-    def __full_random_generation(self) -> list:
-        polynom: list = []
-        for coef in range(self.__degree - len(self.__roots) + 1):
-            if not self.__rational_coefs:
-                polynom.append(random.randint(-20, 21))
-            else:
-                polynom.append(Fraction(random.randint(-20, 21), random.randint(1, 10)))
+    def _full_random_generation(self) -> list:
+        while True:
+            polynom: list = []
+            for coef in range(self._degree - len(self._roots) + 1):
+                if not self._rational_coefs:
+                    polynom.append(
+                        random.randint(poly_settings.int_lowest, poly_settings.int_highest)
+                    )
+                else:
+                    polynom.append(
+                        Fraction(
+                            random.randint(
+                                poly_settings.frac_lowest_num,
+                                poly_settings.frac_highest_num,
+                            ),
+                            random.randint(
+                                poly_settings.frac_lowest_den,
+                                poly_settings.frac_highest_den,
+                            ),
+                        )
+                    )
+            if 0 not in polynom:
+                break
         return polynom
 
-    def __roots_generation(self) -> list:
+    def _roots_generation(self) -> list:
         polynom: list = []
-        for root in self.__roots:
-            if '/' in root:
+        for root in self._roots:
+            if "/" in root:
                 root: Fraction = Fraction(root)
             else:
                 root: int = int(root)
@@ -33,99 +59,140 @@ class PolynomGeneration:
         return polynom
 
     def polynom_constructor(self) -> str:
-        polynom_roots: list = self.__roots_generation()
-        if self.__degree > len(self.__roots):
-            polynom = self.__full_random_generation()
-            if self.__canon_view:
+        polynom_roots: list = self._roots_generation()
+        self.multiplicity_setup(polynom_roots)
+        if self._degree > len(self._roots):
+            polynom = self._full_random_generation()
+            if self._canon_view:
                 for root in polynom_roots:
                     polynom = convolve(polynom, root)
-                return self.__latex(polynom, None)
+                return self._latex(polynom, None)
             else:
-                return self.__latex(polynom, polynom_roots)
-        elif self.__degree == len(self.__roots):
-            if self.__canon_view:
+                return self._latex(polynom, polynom_roots)
+        elif self._degree == len(self._roots):
+            if self._canon_view:
                 polynom = [1]
                 for root in polynom_roots:
                     polynom = convolve(root, polynom)
-                return self.__latex(polynom, None)
+                return self._latex(polynom, None)
             else:
-                return self.__latex(None, polynom_roots)
+                return self._latex(None, polynom_roots)
         else:
             raise Exception("len(roots) > degree")
 
-    def __latex(self, polynom_first, polynom_second) -> str:
-        latex_polynom: str = ''
+    def _latex(self, polynom_first, polynom_second) -> str:
+        latex_polynom: str = ""
+        exceptions: list = []
         if polynom_second is not None:
             for coef in polynom_second:
-                if coef != 0:
-                    numb = self.__latex_coef_modifier(coef[1])
-                    latex_polynom += '(%s%s)' % (self.__variable, numb)
+                if coef not in exceptions:
+                    if coef != 0:
+                        numb = self._latex_coef_modifier(coef[1])
+                        final_bracket: str = (
+                            "(%s%s)" % (self._variable, numb)
+                            if self._multiplicity[coef[1] * -1] == 1
+                            else "(%s%s)^%d"
+                            % (self._variable, numb, self._multiplicity[coef[1] * -1])
+                        )
+                        latex_polynom += final_bracket
+                        exceptions.append(coef)
             if polynom_first is None:
-                return f'\\[{latex_polynom}\\]'
-            if self.__multiplicity > 1:
-                return f'\\[{latex_polynom}^{self.__multiplicity}{self.__latex_canon(polynom_first)}\\]'
+                return f"\\[{latex_polynom}\\]"
             else:
-                return f'\\[{latex_polynom}{self.__latex_canon(polynom_first)}\\]'
+                return f"\\[{latex_polynom}{self._latex_canon(polynom_first)}\\]"
         else:
-            return f'\\[{self.__latex_canon(polynom_first)}\\]'
+            return f"\\[{self._latex_canon(polynom_first)}\\]"
 
-    def __latex_canon(self, polynom: list) -> str:
-        latex_string: str = ''
-        if self.__canon_view:
-            degree_mem: int = self.__degree
+    def _latex_canon(self, polynom: list) -> str:
+        latex_string: str = ""
+        if self._canon_view:
+            degree_mem: int = self._degree
         else:
-            degree_mem: int = self.__degree - len(self.__roots)
+            degree_mem: int = self._degree - len(self._roots)
         current_degree: int = degree_mem
         for coef_orig in polynom:
-            coef: str = self.__latex_coef_modifier(coef_orig)
+            coef: str = self._latex_coef_modifier(coef_orig)
             if coef_orig != 0:
                 if current_degree == degree_mem and current_degree != 1:
-                    coef = self.__plus_minus_one(coef)
-                    if coef != '+':
-                        if coef.startswith('+'):
-                            latex_string += '%s%s^%d' % (coef[1::], self.__variable, current_degree)
+                    coef = self._plus_minus_one(coef)
+                    if coef != "+":
+                        if coef.startswith("+"):
+                            latex_string += "%s%s^%d" % (
+                                coef[1::],
+                                self._variable,
+                                current_degree,
+                            )
                         else:
-                            latex_string += '%s%s^%d' % (coef, self.__variable, current_degree)
+                            latex_string += "%s%s^%d" % (
+                                coef,
+                                self._variable,
+                                current_degree,
+                            )
                     else:
-                        latex_string += '%s^%d' % (self.__variable, current_degree)
+                        latex_string += "%s^%d" % (self._variable, current_degree)
                 elif current_degree > 1:
-                    coef = self.__plus_minus_one(coef)
-                    latex_string += '%s%s^%d' % (coef, self.__variable, current_degree)
+                    coef = self._plus_minus_one(coef)
+                    latex_string += "%s%s^%d" % (coef, self._variable, current_degree)
                 elif current_degree == 1:
-                    coef = self.__plus_minus_one(coef)
+                    coef = self._plus_minus_one(coef)
                     if degree_mem == 1:
-                        if coef.startswith('+'):
+                        if coef.startswith("+"):
                             coef = coef[1::]
-                    latex_string += '%s%s' % (coef, self.__variable)
+                    latex_string += "%s%s" % (coef, self._variable)
                 elif current_degree == 0:
-                    latex_string += '%s' % coef
+                    latex_string += "%s" % coef
             current_degree -= 1
-        if self.__canon_view:
+        if self._canon_view:
             return latex_string
         else:
-            return f'({latex_string})'
+            return f"({latex_string})"
+
+    def multiplicity_setup(self, polynom_roots: list) -> None:
+        multip_dict: dict = {}
+        for root in polynom_roots:
+            multip_dict[root[1] * -1] = multip_dict.get(root[1] * -1, 0) + 1
+        for value in multip_dict:
+            if type(value) == Fraction:
+                value_str: str = f"{value.numerator}/{value.denominator}"
+            else:
+                value_str: str = str(value)
+            try:
+                if self._multiplicity[value_str] > 1:
+                    multip_dict[value] = (
+                        multip_dict.get(value, 0) + self._multiplicity[value_str] - 1
+                    )
+            except KeyError:
+                continue
+        polynom_roots.clear()
+        self._roots.clear()
+        self._multiplicity = multip_dict
+        for multi in self._multiplicity:
+            for times in range(0, self._multiplicity[multi]):
+                polynom_roots.append([1, multi * -1])
+                if type(multi) == int:
+                    self._roots.append(str(multi))
+                elif type(multi) == Fraction:
+                    self._roots.append(f"{multi.numerator}/{multi.denominator}")
+        if self._degree < len(polynom_roots):
+            raise Exception("degree error( degree < len(roots) )")
 
     @staticmethod
-    def __latex_coef_modifier(numb) -> str:
+    def _latex_coef_modifier(numb) -> str:
         if type(numb) == Fraction:
             if numb.denominator == 1:
                 numb = numb.numerator
         if type(numb) == Fraction:
             if numb.numerator < 0:
-                return '-\\frac{%d}{%d}' % (numb.numerator * -1, numb.denominator)
+                return "-\\frac{%d}{%d}" % (numb.numerator * -1, numb.denominator)
             else:
-                return '+\\frac{%d}{%d}' % (numb.numerator, numb.denominator)
+                return "+\\frac{%d}{%d}" % (numb.numerator, numb.denominator)
         else:
-            return '{:+}'.format(numb)
+            return "{:+}".format(numb)
 
     @staticmethod
-    def __plus_minus_one(coef: str) -> str:
-        if coef == '+1':
-            return '+'
-        elif coef == '-1':
-            return '-'
+    def _plus_minus_one(coef: str) -> str:
+        if coef == "+1":
+            return "+"
+        elif coef == "-1":
+            return "-"
         return coef
-
-
-#a = PolynomGeneration(3, ['3/1', '1/3'], False, 1, True, 'x')
-#print(a.polynom_constructor())
---
+++ generating_tasks_lib/poly_settings.py
@@ -0,0 +1,6 @@
+int_lowest = -1
+int_highest = 1
+frac_lowest_num = -10
+frac_highest_num = 10
+frac_lowest_den = -10
+frac_highest_den = 10
