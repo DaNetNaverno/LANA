340ad443e8e94d59a262a2c4760bb9e25512b40a
Alex <alesha200103@mail.ru>
2022-11-10 20:40:13 +0300

    fix

--- generating_tasks_lib/poly_settings.py
+++
@@ -1,6 +0,0 @@
-int_lowest = -20
-int_highest = 20
-frac_lowest_num = -30
-frac_highest_num = 30
-frac_lowest_den = -10
-frac_highest_den = 10
--- generating_tasks_lib/polynom_generation/poly_settings.py
+++ generating_tasks_lib/polynom_generation/poly_settings.py
@@ -2,9 +2,9 @@ from pydantic import BaseSettings
 
 
 class PolynomGenerationSettings(BaseSettings):
-    int_lowest: int = -1
-    int_highest: int = 1
-    frac_lowest_num: int = -10
-    frac_highest_num: int = 10
+    int_lowest: int = -20
+    int_highest: int = 20
+    frac_lowest_num: int = -30
+    frac_highest_num: int = 30
     frac_lowest_den: int = -10
     frac_highest_den: int = 10
